3d6dea210a91f4c8effe63d8ddd21e1de39b1ee7
Jayesh Pramod mondkar <10120288@JDial.com>
2026-04-20 11:52:34 +0530

    Initial GeoForge release

--- src/geoforge/engine/entity.py
+++ src/geoforge/engine/entity.py
@@ -1,7 +1,14 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    nlp = None
+
 
 def extract_entities(text):
+    if not nlp:
+        return []
+
     doc = nlp(text)
     return list(set([ent.text for ent in doc.ents]))
